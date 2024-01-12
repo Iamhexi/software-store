9278245bf240a9a82ace1583b70bca00e587bf4a
Iamhexi <igor.sosnowicz@gmail.com>
2024-01-12 13:43:11 +0100

    implement Request middleware functionality, fix auth bug

--- backend/public/index.php
+++ backend/public/index.php
@@ -2,51 +2,53 @@
 require_once __DIR__.'/../app/controller/UserController.php';
 require_once __DIR__.'/../app/middleware/AuthenticationService.php';
 require_once __DIR__.'/../app/middleware/AuthorizationService.php';
+require_once __DIR__.'/../app/middleware/Request.php';
 require_once __DIR__.'/../app/middleware/Endpoint.php';
 
-
-$path = $_SERVER['REQUEST_URI'];
-$endpoint = Endpoint::fromString(explode('/', $path)[2]); // TODO: remove query params from the path!
-$method = strtolower($_SERVER['REQUEST_METHOD']);
+$endpoint = Request::get_endpoint();
+$method = Request::get_request_method();
 
 
-// Obtaining a bearer token
-$login = $_GET['login'];
-$password = $_GET['password'];
+switch ($endpoint) {
+    case Endpoint::Auth: // authentication with a login and password to obtain a bearer token
+        
+        $login = Request::get_query_parameter('login');
+        $password = Request::get_query_parameter('password');
+        
+        if ($method !== 'post') 
+            Controller::send_response(405, 'Failure', 'Invalid method');
 
-// Authentication with a login and password to obtain a bearer token. Endpoint: Auth
-
-$authentication_service = new AuthenticationService;
-if (isset($login) && isset($password) && !empty($login) && !empty($password)) {
-    if ($method === 'post' && $endpoint === Endpoint::Auth) {
+        $authentication_service = new AuthenticationService;
         $result = $authentication_service->authenticate($login, $password);
         if ($result === false)
             Controller::send_response(401, 'Failure', 'Invalid login or/and password');
         else
             Controller::send_response(200, 'Success', $result);
-    } else {
-        Controller::send_response(405, 'Failure', 'Invalid method');
-    }
-} 
 
-else if ($endpoint === Endpoint::User && $method === 'post') { // registration
-    $controller = new UserController;
-    $controller->handle_request(); 
+        break;
+    case Endpoint::User:
+        if ($method === 'post') { // handles only registration (post) without a bearer token, otherwise requires a bearer token
+            $controller = new UserController;
+            $controller->handle_request();
+        } 
+        break;
+
+    default:
+        break;
 }
 
-
 // Authentication with the obtained bearer token
+$authentication_service = new AuthenticationService;
 if (($token = $authentication_service->get_bearer_token()) === null)
-    Controller::send_response(401, 'Failure', 'Invalid bearer token');
+    Controller::send_response(401, 'Failure', 'Missing or invalid bearer token');
 
 if (!$authentication_service->verify_token($token))
-    Controller::send_response(401, 'Failure', 'Expired or non-existent bearer token');
+    Controller::send_response(401, 'Failure', 'Expired bearer token');
 
 // Authorization
 $authorization_service = new AuthorizationService;
 if (!$authorization_service->authorize($token, $endpoint))
     Controller::send_response(403, 'Failure', 'Access forbidden. Insufficient privileges');
-
 
 
 // Routing
