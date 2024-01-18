4a3b744d67f27c38b0c4dcf392252dcc833970d0
Iamhexi <igor.sosnowicz@gmail.com>
2024-01-18 22:04:19 +0100

    report bug, add admin account to the default db schema

--- backend/public/index.php
+++ backend/public/index.php
@@ -32,7 +32,9 @@ switch ($endpoint) {
         break;
 
     case Endpoint::User:
-        if ($method === 'post' && $request->get_path_parameter(2) === null) { // handles only registration (post) without a bearer token, otherwise requires a bearer token
+        // Registration without a bearer token
+        // TODO: it's BROKEN! remeber you cannot just remove the second condition as it allows the subcontrollers to handle requests
+        if ($method === 'post' && ($request->get_path_parameter(2) === null || !is_numeric($request->get_path_parameter(2)))) { // handles only registration (post) without a bearer token, otherwise requires a bearer token
             $controller = new UserController;
             $controller->handle_request($request);
         } 
